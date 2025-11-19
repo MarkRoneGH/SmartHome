b72298f0591d34839b0adc6a4012fe1189879563
Mark Rone <markshotn1@gmail.com>
2025-11-19 21:42:07 +0300

    Изменил кодировку

--- CP_OOPiP/main.cpp
+++ CP_OOPiP/main.cpp
@@ -5,8 +5,8 @@ using smart_system::SmartHomeInteraction;
 int main()
 {
 	system("color f0");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
 
 	try {
 		SmartHomeInteraction::showEntryMenu();
